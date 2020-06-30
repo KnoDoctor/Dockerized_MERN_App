86ef93d65f05e7f792d46bc574220726598cd46a
Jeff Barfield <jeff.barfield@butterfield.com>
2020-06-30 18:37:59 -0400

    REmoved Live Branding
    Signed-off-by: Jeff Barfield <jeff.barfield@butterfield.com>

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -312,7 +312,7 @@ export default function Header(props) {
                                 }}
                                 to="/"
                             >
-                                EPOCH North (Live)
+                                EPOCH North
                             </Link>
                         </Typography>
                         <div className={classes.search}>
