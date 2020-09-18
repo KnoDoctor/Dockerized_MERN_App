5e295708173ea2822128f33f5d0812470ef265b9
Jeff Barfield <jeff.barfield@butterfield.com>
2020-09-18 17:08:25 -0400

    Signed-off-by: Jeff Barfield <jeff.barfield@butterfield.com>

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -66,20 +66,9 @@ exports.addPage = async (req, res, next) => {
             data: page,
         });
     } catch (err) {
-        if (err.name === "ValidationError") {
-            const messages = Object.values(err.errors).map(
-                (val) => val.message
-            );
-
-            return res.status(400).json({
-                success: false,
-                error: messages,
-            });
-        } else {
-            return res.status(500).json({
-                success: false,
-                error: "Server Error",
-            });
-        }
+        return res.status(500).json({
+            success: false,
+            error: "Server Error",
+        });
     }
 };
